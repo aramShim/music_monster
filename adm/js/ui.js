3a1c8040045898e4fc12a50d182e0fde8e905bc8
arams <oeo8909@naver.com>
2024-02-06 16:44:50 +0900

    앨범발매 view

--- adm/js/ui.js
+++ adm/js/ui.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
   mypageSelect();
+  tab();
+  
 });
 
 function mypageSelect() {
@@ -10,4 +12,28 @@ function mypageSelect() {
   $langWrap.on("mouseleave", function () {
     $(this).removeClass("active");
   });
+}
+
+function popup(url, name, width, height){
+  var width = width;
+  var height = height;
+  var option = "width = "+width+", height = "+height+", top = 100, left = 200, location = no"
+  window.open(url, name, option);
+}
+
+function tab(){
+  $('.js-tabgroup > div').hide();
+	$('.js-tabgroup > div:first-of-type').show();
+	$('.js-tabs a').click(function(e){
+	  e.preventDefault();
+		var $this = $(this),
+        tabgroup = '#'+$this.parents('.js-tabs').data('tabgroup'),
+        others = $this.closest('li').siblings().children('a'),
+        target = $this.attr('href');
+
+		others.removeClass('active');
+		$this.addClass('active');
+		$(tabgroup).children('div').hide();
+		$(target).show();  
+	})
 }
